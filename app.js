691d2865adb0d125f2ce4992d059324600656278
Ashutosh Kumar <ashutoshkumar44549@gmail.com>
2026-02-07 15:59:23 +0530

    How task is added is done

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-let inp = document.getElementById("section #input");
-let btn = document.getElementsByClassName("section .button");
+let inp = document.getElementById("input");
+let btn = document.getElementById("button");
+let taskList = document.getElementById("taskList");
 
 let tasks = [];
+
+btn.addEventListener("click", () => {
+    let userInput = inp.value;
+    if (!userInput.trim()) {
+        console.log("No tasks to add");
+    } else {
+        let lists = document.createElement("li");
+        lists.innerText = userInput;
+        taskList.appendChild(lists);
+        inp.value = "";
+        tasks.push(userInput);
+        console.log(tasks);
+    }
+
+})
+
+
