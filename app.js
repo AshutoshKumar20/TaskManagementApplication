f199fba316b9c42e9a8e038b03b0530f4af5d160
Ashutosh Kumar <ashutoshkumar44549@gmail.com>
2026-03-15 09:26:55 +0530

    Separated Logic and UI

--- app.js
+++ app.js
@@ -4,35 +4,35 @@ let taskList = document.getElementById("taskList");
 
 let tasks = [];
 
-let renderTasks = () => {
-    let userInput = inp.value;
-    if (!userInput.trim()) {
-        console.log("No tasks to add");
-    } else {
-
-        let btn2 = document.createElement("button");
-        btn2.textContent = "Delete a Task";
-        lists.appendChild(btn2);
-        btn2.addEventListener("click", () => {
-            let index = tasks.indexOf(userInput);
-            if (index !== -1) {
-                tasks.splice(index, 1);
-            }
-            lists.remove();
-            console.log(tasks);
-        })
-        console.log(tasks);
-    }
-}
-btn.addEventListener("click", () => {
+let renderTasks = (userInput) => {
     let lists = document.createElement("li");
     lists.innerText = userInput;
     taskList.appendChild(lists);
     inp.value = "";
+
+    let btn2 = document.createElement("button");
+    btn2.textContent = "Delete a Task";
+    lists.appendChild(btn2);
+    btn2.addEventListener("click", () => {
+        let index = tasks.indexOf(userInput);
+        if (index !== -1) {
+            tasks.splice(index, 1);
+        }
+        lists.remove();
+        console.log(tasks);
+    })
+    console.log(tasks);
+}
+
+btn.addEventListener("click", () => {
+    let userInput = inp.value;
+    if (!userInput.trim()) {
+        console.log("No tasks to add");
+    }
     tasks.push(userInput);
-    renderTasks();
+    renderTasks(userInput);
 });
-btn2.addEventListener("click", () => { })
+
 
 
 
